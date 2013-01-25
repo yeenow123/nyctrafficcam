17073b4c114850f0ea3ba2b74151efa68c40015e
Yee-Ning <yee.ningc@gmail.com>
2013-01-25 01:19:45 -0500

    Rails app generated

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Nyctrafficcam::Application.config.secret_token = 'da45b488b8168d4318da51c6bf824e2228c63c14cf0362c17a14674002e8da7e9f0377ba06566d341f7b19b539f9797e540f643b1ac231b56d03fae6194010b6'
