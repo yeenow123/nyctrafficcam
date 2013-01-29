377d1751fdd079370c3a86223649f692f1e5c055
Yee-Ning Cheng <yee.ningc@gmail.com>
2013-01-29 17:54:16 -0500

    Shitty data made wrong lat/long points!, should use polyline encoder instead?

--- app/controllers/maps_controller.rb
+++ app/controllers/maps_controller.rb
@@ -1,5 +1,7 @@
 class MapsController < ApplicationController
 	def index
+		#ids 160 (linkpoint id is 3170, delete 3169) lat should be 40.797
+		#169 latitude should be: 40.80666
 		all_points = []
 		Link.includes(:link_points).all.each do |link|
 			all_points << link.link_points.map { |pt| { lat: pt.latitude, lng: pt.longitude } }
